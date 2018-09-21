34f0377e06c8abfcb1000c28f0f6f29398174c98
TheDude70 <dude.the.man@gmail.com>
2018-09-21 17:15:45 +0800

    upcast custom textcolor

--- src/textcolor/textcolorediting.js
+++ src/textcolor/textcolorediting.js
@@ -1,11 +1,23 @@
 import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
 
-import {downcastAttributeToElement} from '@ckeditor/ckeditor5-engine/src/conversion/downcast-converters';
 import {upcastElementToAttribute} from '@ckeditor/ckeditor5-engine/src/conversion/upcast-converters';
 import TextColorCommand from './textcolorcommand';
 import Colors from '../colors.js';
 
 const TEXT_COLOR = 'textColor';
+
+Array.prototype.closest = function (str) {
+        let min = 0xffffff;
+        let best, current, i;
+        for (i = 0; i < this.length; i++) {
+            current = dist(this[i].color.substr(1), str.substr(1));
+            if (current < min) {
+                min = current;
+                best = this[i];
+            }
+        }
+        return best;
+};
 
 export default class TextColorEditing extends Plugin {
     /**
@@ -34,6 +46,41 @@ export default class TextColorEditing extends Plugin {
         // Set-up the two-way conversion.
         editor.conversion.attributeToElement(definition);
 
+        editor.conversion.for('upcast')
+            .add(upcastElementToAttribute({
+                view: {
+                    name: 'span',
+                    attributes: {
+                        title: 'color'
+                    }
+                },
+                model: {
+                    key: TEXT_COLOR,
+                    value: viewElement => {
+                        const color = viewElement.getStyle('color');
+
+                        if (!color) {
+                            return null
+                        }
+
+                        // determine closest color
+                        const regEx = /^rgb\((\d*),\s+(\d*),\s+(\d*)\)$/;
+                        let rgb = regEx.exec(color);
+                        console.log(rgb);
+                        if (!rgb) {
+                            return null;
+                        }
+                        let best = options.closest(toHex(rgb[1], rgb[2], rgb[3]));
+
+                        if (!best) {
+                            return null;
+                        }
+
+                        return best.model;
+                    }
+                }
+            }));
+
         // Add TextColor command.
         editor.commands.add(TEXT_COLOR, new TextColorCommand(editor));
     }
@@ -45,7 +92,8 @@ function _buildDefinition(options) {
             key: TEXT_COLOR,
             values: []
         },
-        view: {}
+        view: {},
+        upcastAlso: {}
     };
 
     for (const option of options) {
@@ -56,7 +104,7 @@ function _buildDefinition(options) {
                 'color': option.model
             },
             priority: 5
-        }
+        };
     }
 
     return definition;
@@ -70,4 +118,14 @@ function toRGB(color) {
     let b = parseInt(hex.substr(4, 2), 16);
 
     return 'rgb(' + r + ',' + g + ',' + b + ')';
+}
+
+function toHex(r, g, b) {
+    return '#' + parseInt(r).toString(16) + parseInt(g).toString(16) + parseInt(b).toString(16);
+}
+
+function dist(s, t) {
+    if (!s.length || !t.length) return 0;
+    return dist(s.slice(2), t.slice(2)) +
+        Math.abs(parseInt(s.slice(0, 2), 16) - parseInt(t.slice(0, 2), 16));
 }
